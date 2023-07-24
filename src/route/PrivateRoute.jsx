45abf58bfbaecfea23d396e6a4219afb1b253f34
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-07-25 00:04:08 +0600

    work is done

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,18 +1,14 @@
 
 import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router';
-import { Spinner } from 'react-bootstrap';
 import { AuthContext } from '../provider/AuthProvider';
 
 
 
 const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+    const {user} = useContext(AuthContext);
     const location = useLocation();
     console.log('user in private route', user);
-    if(loading){
-        return <Spinner animation="border" variant="primary" />
-    }
 
     if(user){
         return children;
